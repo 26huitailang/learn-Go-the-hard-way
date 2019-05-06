f3f6cbb3c678ea036846a190bf2b2bb727171016
26huitailang <26huitailang@gmail.com>
2019-05-06 16:42:20 +0800

    finish l2

--- main.go
+++ main.go
@@ -52,7 +52,28 @@ func ParallelSum(slcs ...[]int) []int {
 }
 
 //TODO:complete the Sum for the parallel sum function.
-var Sum func(sum chan []int) (output chan []int)
+// var Sum func(sum chan []int) (output chan []int)
+
+// Sum 每次取两个vector相加返回结果
+func Sum(sum chan []int) (output chan []int) {
+	output = make(chan []int) // 参数有声明，还是要初始化
+	go func() {
+		var tmp []int
+		for vecotr := range sum {
+			if len(tmp) == 0 {
+				tmp = append(tmp, vecotr...)
+				continue
+			}
+			for i, v := range vecotr {
+				tmp[i] += v
+			}
+		}
+		fmt.Println(tmp)
+		output <- tmp
+		defer close(output)
+	}()
+	return output
+}
 
 func main() {
 	fmt.Println(`Please edit main.go,and complete the 'Sum' function for the parallel sum to pass the test.
@@ -61,6 +82,5 @@ Concurrency is the most important feature of Go,and the principle is
 In this exercise you need to catch many features of channels.This is a tour for you to figure out!
 Because here the focus is pipleline model (link:http://blog.golang.org/pipelines).
 It's different from the custom parallel vector sum in which sum numer at every index of the vectors in a goroutine.
-In this exercies,vector is just a abstract,you can change it to a struct or any thing else that can be sumed up.
-`)
+In this exercies,vector is just a abstract,you can change it to a struct or any thing else that can be sumed up.`)
 }
